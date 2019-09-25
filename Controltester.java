6e76906bf26495e0fb5caa247787432f4e7d8a35
khamtran411 <44482407+khamtran411@users.noreply.github.com>
2019-09-25 10:40:20 -0700

    Add files via upload

---
+++ Controltester.java
@@ -0,0 +1,27 @@
+import java.io.FileNotFoundException;
+
+public class Controltester {
+	public static void main(String[] args) throws FileNotFoundException {
+		Control test = new Control();
+
+		test.printOutput();
+		System.out.print(test.rowTest(0, 6));
+		System.out.print(test.colTest(2, 4));
+		System.out.print(test.sectionTest(3,9));
+		test.setElement(0, 0, "9");
+		System.out.println();
+		test.printOutput();
+		
+		System.out.println();
+		test.printOutput();
+		//		int x= 4;
+		//		
+		//		if(test.print().equals(Integer.toString(x)))
+		//		{
+		//			
+		//		}
+		test.rePlay();
+		System.out.println();
+		test.printOutput();
+	}
+}
---
+++ Model.java
@@ -0,0 +1,11 @@
+import java.io.FileNotFoundException;
+
+public class Model {
+private Control c;
+
+Model() throws FileNotFoundException{
+	c = new Control();
+}
+
+
+}
